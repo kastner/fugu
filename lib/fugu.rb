8e0d6d27860c0534346187fc6e10ad6d857d58c0
Ian Meyer <ianmmeyer@gmail.com>
2009-12-27 03:19:08 +0800

    New feature for {1-3} expansion

--- lib/fugu.rb
+++ lib/fugu.rb
@@ -5,6 +5,7 @@ class Fugu
     if @text.match(/\{/)
       before, expand_string, after = @text.scan(/(.*)(\{.*\})(.*)/)[0]
       expanded_string = expand_expression(expand_string)
+      raise expanded_string.inspect
       @text = expanded_string.map { |piece| before + piece + after }.join(",")
     end
   end
@@ -13,6 +14,12 @@ class Fugu
     out = string.dup
     out.gsub!(/[\{\}]/, '')
     range = out.split(",")
+    Range.new(*range).to_a
+  end
+
+  def expand_range(string)
+    out = string.dup
+    out.gsub!(/[\-]/, '..')
     Range.new(*range).to_a
   end
 end
