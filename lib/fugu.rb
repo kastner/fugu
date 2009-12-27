9d88b9807632d88418367fcbdff7e758f0316e41
Erik Kastner <kastner@gmail.com>
2009-12-27 09:37:22 -0500

    simplified

--- lib/fugu.rb
+++ lib/fugu.rb
@@ -13,19 +13,13 @@ class Fugu
     out = string.dup
     out.gsub!(/[\{\}]/, '')
     tmp_a = []
-    if out =~ /,/
-      out.split(",").each do |v|
-        # Test for a range
-        if v =~ /-/
-          tmp_a << Range.new(*v.split('-')).to_a
-        else
-          tmp_a << v
-        end
+    out.split(",").each do |v|
+      # Test for a range
+      if v =~ /-/
+        tmp_a << Range.new(*v.split('-')).to_a
+      else
+        tmp_a << v
       end
-    elsif out =~ /-/
-      tmp_a << Range.new(*out.split('-')).to_a
-    else
-      tmp_a << out
     end
     return tmp_a.flatten
   end
