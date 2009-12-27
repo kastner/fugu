811dfbf24b60fb7d754035142567542d28cc55af
Ian Meyer <imeyer@little-air.local>
2009-12-27 01:39:43 -0500

    More features that PASS! WOO!

--- features/fugu.feature
+++ features/fugu.feature
@@ -7,6 +7,11 @@ Feature: Fugu rules
     Given the string "web0001"
     When I expand it
     Then I should get back "web0001"
+
+  Scenario: Unexpandable Fugu expression comes back as it went in
+    Given the string "web{0001}"
+    When I expand it
+    Then I should get back "web0001"
   
   Scenario: Simple expansion
     Given the string "web000{1,2}"
@@ -17,3 +22,8 @@ Feature: Fugu rules
     Given the string "web000{1-3}"
     When I expand it
     Then I should get back "web0001,web0002,web0003"
+
+  Scenario: Mix of simple and complex ranges
+    Given the string "web000{1,2-4,6}"
+    When I expand it
+    Then I should get back "web0001,web0002,web0003,web0004,web0006"
--- lib/fugu.rb
+++ lib/fugu.rb
@@ -5,7 +5,6 @@ class Fugu
     if @text.match(/\{/)
       before, expand_string, after = @text.scan(/(.*)(\{.*\})(.*)/)[0]
       expanded_string = expand_expression(expand_string)
-      raise expanded_string.inspect
       @text = expanded_string.map { |piece| before + piece + after }.join(",")
     end
   end
@@ -13,13 +12,22 @@ class Fugu
   def expand_expression(string)
     out = string.dup
     out.gsub!(/[\{\}]/, '')
-    range = out.split(",")
-    Range.new(*range).to_a
-  end
-
-  def expand_range(string)
-    out = string.dup
-    out.gsub!(/[\-]/, '..')
-    Range.new(*range).to_a
+    tmp_a = []
+    if out =~ /,/
+      out.split(",").each do |v|
+        # Test for a range
+        if v =~ /-/
+          tmp_a << Range.new(*v.split('-')).to_a
+        else
+          tmp_a << v
+        end
+      end
+    elsif out =~ /-/
+      tmp_a << Range.new(*out.split('-')).to_a
+    else
+      tmp_a << out
+    end
+    return tmp_a.flatten
   end
 end
+
