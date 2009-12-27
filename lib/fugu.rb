35fc42875f8331adaf9d4dd174bad72ad80e7aa8
Ian Meyer <imeyer@little-air.local>
2009-12-28 03:12:25 +0800

    Added another scenario

--- lib/fugu.rb
+++ lib/fugu.rb
@@ -2,11 +2,25 @@ class Fugu
   attr_accessor :text
   
   def expand
-    if @text.match(/\{/)
-      before, expand_string, after = @text.scan(/(.*)(\{.*\})(.*)/)[0]
-      expanded_string = expand_expression(expand_string)
-      @text = expanded_string.map { |piece| before + piece + after }.join(",")
+    tmp_a = []
+    if @text[/\|/]
+      @text.split('|').each do |range|
+        if range.match(/\{/)
+          before, expand_string, after = range.scan(/(.*)(\{.*\})(.*)/)[0]
+          expanded_string = expand_expression(expand_string)
+          tmp_a << expanded_string.map { |piece| before + piece + after }
+        else
+          tmp_a << range
+        end
+      end
+    else
+      if @text.match(/\{/)
+        before, expand_string, after = @text.scan(/(.*)(\{.*\})(.*)/)[0]
+        expanded_string = expand_expression(expand_string)
+        tmp_a << expanded_string.map { |piece| before + piece + after }
+      end
     end
+    return tmp_a.flatten.join(',')
   end
   
   def expand_expression(string)
