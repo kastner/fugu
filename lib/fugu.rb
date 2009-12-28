8519e6b78fc9fd0d2b017674fca5f7a3ace72cd1
Erik Kastner <kastner@gmail.com>
2009-12-27 23:21:19 -0500

    refactor and change API slightly (taking out {)

--- lib/fugu.rb
+++ lib/fugu.rb
@@ -6,7 +6,7 @@ class Fugu
   def puff
     @text.split('|').collect do |range|
       if range.match(/\{/)
-        before, expand_string, after = range.scan(/(.*)(\{.*\})(.*)/)[0]
+        before, expand_string, after = range.scan(/(.*)\{(.*)\}(.*)/)[0]
         expanded_string = puff_expression(expand_string)
         range = expanded_string.map { |piece| before + piece + after }
       end
@@ -17,38 +17,37 @@ class Fugu
   def puff!
     @text = puff
   end
-
+  
   def puff_expression(string)
-    string.gsub(/[\{\}]/, '').split(",").inject([]) do |a,v|
+    string.split(",").inject([]) do |a,v|
       v = Range.new(*v.split('-')).to_a if v[/-/]
       a << v
     end.flatten
   end
   
   def shrink
-    @text = shrink_expression(@text)
-  end
-
-  def shrink!
-    @text = shrink
-  end
-
-  def shrink_expression(string)
-    pieces = string.split(",")
+    pieces = @text.split(",")
     first, base = pieces.first, pieces.first
     first.size.times do |len|
       base = first[0, first.size-len]
       break if pieces.all? {|p| p.match(base)}
     end
     diffs = pieces.map {|p| (p.scan(/./) - base.scan(/./)).join}
-    extras = diffs.flatten.to_ranges.map do |r|
+    base + "{#{shrink_array(diffs)}}"
+  end
+  
+  def shrink!
+    @text = shrink
+  end
+  
+  def shrink_array(array)
+    array.flatten.to_ranges.map do |r|
       if r.first == r.last
         r.first
       else
         "#{r.first}-#{r.last}"
       end
     end.join(",")
-    base + "{#{extras}}"
-  end  
+  end
 end
 
--- test/fugu_test.rb
+++ test/fugu_test.rb
@@ -6,10 +6,10 @@ class FuguTest < Test::Unit::TestCase
   end
   
   should "puff a string like {1-3}" do
-    assert_equal %w[1 2 3], @fugu.puff_expression("{1-3}")
+    assert_equal %w[1 2 3], @fugu.puff_expression("1-3")
   end
 
   should "shrink a string like 1,2,3" do
-    assert_equal "{1-3}", @fugu.shrink("1,2,3")
+    assert_equal "1-3", @fugu.shrink_array(%w[1 2 3])
   end
 end
